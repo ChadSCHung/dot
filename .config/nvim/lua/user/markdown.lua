8393bef13ad8bf05c5c2038c22e2e88c0b3a01bc
ChadSCHung <chad.shu.chun.hung@gmail.com>
2025-01-19 22:00:58 +0800

    nvim: add keybind for toggle rendering markdown

--- .config/nvim/lua/user/markdown.lua
+++ .config/nvim/lua/user/markdown.lua
@@ -1,6 +1,7 @@
 local M = {
     'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
+    opts = { enabled = false, anti_conceal = { enabled = false } },
+    keys = { { '<leader>mr', '<cmd>RenderMarkdown toggle<cr>', desc = 'Render markdown file in buffer'}},
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
