d87fa02be6cdbab196ab550d199184fe040e1f95
ChadSCHung <chad.shu.chun.hung@gmail.com>
2025-01-19 22:00:31 +0800

    nvim: use new window of browser for markdown preview

--- .config/nvim/lua/user/markdown-preview.lua
+++ .config/nvim/lua/user/markdown-preview.lua
@@ -1,7 +1,31 @@
-local M ={
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-} 
+local M = {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
+
+function M.config()
+  vim.cmd [[
+
+function OpenMarkdownPreview (url)
+
+let cmd = "export DISPLAY=:0;zen-browser --new-window " . shellescape(a:url) . " &"
+
+silent call system(cmd)
+
+endfunction
+
+]]
+  vim.g.mkdp_auto_start = 1
+  vim.g.mkdp_auto_close = 0
+  vim.g.mkdp_command_for_global = 0
+  vim.g.mkdp_open_to_the_world = 1
+  vim.g.combine_preview = 1
+  vim.g.combine_preview_auto_refresh = 1
+  vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+end
+
 return M
