ed9780ca5a5d9eff23790eb528aa0d9eabef4a6b
ChadSCHung <chad.shu.chun.hung@gmail.com>
2025-01-19 22:01:45 +0800

    nvim: show image only when cursor move to the line

--- .config/nvim/lua/user/image.lua
+++ .config/nvim/lua/user/image.lua
@@ -17,9 +17,9 @@ local M = {
             integrations = {
                 markdown = {
                     enabled = true,
-                    clear_in_insert_mode = false,
+                    clear_in_insert_mode = true,
                     download_remote_images = true,
-                    only_render_image_at_cursor = false,
+                    only_render_image_at_cursor = true,
                     filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
                     -- resolve_image_path = function(document_path, image_path, fallback)
                     --     -- document_path is the path to the file that contains the image
@@ -31,7 +31,7 @@ local M = {
                     -- end,
                 },
                 neorg = {
-                    enabled = true,
+                    enabled = false,
                     clear_in_insert_mode = false,
                     download_remote_images = true,
                     only_render_image_at_cursor = false,
